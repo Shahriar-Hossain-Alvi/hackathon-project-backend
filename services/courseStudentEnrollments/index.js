d6411cc3e0fa4d14f79409adc63cce0813860301
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2024-12-09 19:07:10 +0600

    added api to get all course student enrollment

--- services/courseStudentEnrollments/index.js
+++ services/courseStudentEnrollments/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 // Create a new course student enrollment 
 router.post("/", require("./views/create-courseStudentEnrollment"));
 
+// get all course student enrollment with populate data
+router.get("/", require("./views/get-courseStudentEnrollments"));
 
 module.exports = router;
---
+++ services/courseStudentEnrollments/views/get-courseStudentEnrollments.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const CourseStudentEnrollment = require('../schema/courseStudentEnrollment.schema');
+
+
+module.exports = async (req, res, next) => {
+    try {
+        // Fetch all course student enrollments with populated user and course details
+        const enrollments = await CourseStudentEnrollment.find()
+            .populate("users_id", "first_name email") // Populate the user's first name and email
+            .populate("course_id", "title description"); // Populate the course title and description
+
+        // If no enrollments found
+        if (enrollments.length === 0) {
+            return next(new ErrorResponse("No student enrollments found.", 404));
+        }
+
+        // Send success response
+        res.status(200).json({
+            success: true,
+            message: "Student enrollments fetched successfully.",
+            data: enrollments,
+        });
+
+    } catch (error) {
+        // send error response
+        next(error)
+    }
+}
